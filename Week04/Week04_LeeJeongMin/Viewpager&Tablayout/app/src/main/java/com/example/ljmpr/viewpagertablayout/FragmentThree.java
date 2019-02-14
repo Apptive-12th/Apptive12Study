109cac574994abd2da709069e989c9cfac890606
A-Jeong <ljmpretty98@gmail.com>
2019-02-15 03:21:39 +0900

    Week04

---
+++ Week04/Week04_LeeJeongMin/Viewpager&Tablayout/app/src/main/java/com/example/ljmpr/viewpagertablayout/FragmentThree.java
@@ -0,0 +1,29 @@
+package com.example.ljmpr.viewpagertablayout;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+public class FragmentThree extends Fragment {
+
+    public FragmentThree() {
+
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view;
+        view = inflater.inflate(R.layout.fragment_three,null);
+        return view;
+    }
+}
