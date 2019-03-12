43579dd67e850b88437368e3738d4be3237ec4ac
SonDongHyun <sondonghyun3@naver.com>
2019-03-13 00:09:44 +0900

    TT
    TT

---
+++ Week05/hw_week6_SDH/app/src/main/java/com/example/week6_sdh/Fragment_Camera.java
@@ -0,0 +1,58 @@
+package com.example.week6_sdh;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Fragment_Camera extends Fragment {
+    View v;
+    private RecyclerView myrecyclerview ;
+    private List<Contact> lstContact;
+
+    public Fragment_Camera() {
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        v= inflater.inflate(R.layout.fragment_camera,container, false);
+        myrecyclerview = (RecyclerView) v.findViewById(R.id.contact_recyclerview);
+
+        RecyclerViewAdapter recyclerAdapter = new RecyclerViewAdapter(getContext(),lstContact);
+        myrecyclerview.setLayoutManager(new LinearLayoutManager(getActivity(),LinearLayoutManager.HORIZONTAL,false));
+        myrecyclerview.setAdapter(recyclerAdapter);
+        return  v;
+
+
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        lstContact = new ArrayList<>();
+        lstContact.add(new Contact("AAA","123450",R.drawable.person));
+        lstContact.add(new Contact("BBB","654321",R.drawable.person2));
+        lstContact.add(new Contact("CCC","123450",R.drawable.person));
+        lstContact.add(new Contact("DDD","654321",R.drawable.person2));
+        lstContact.add(new Contact("EEE","123450",R.drawable.person));
+        lstContact.add(new Contact("FFF","654321",R.drawable.person2));
+        lstContact.add(new Contact("AAA","123450",R.drawable.person));
+        lstContact.add(new Contact("BBB","654321",R.drawable.person2));
+        lstContact.add(new Contact("CCC","123450",R.drawable.person));
+        lstContact.add(new Contact("DDD","654321",R.drawable.person2));
+        lstContact.add(new Contact("EEE","123450",R.drawable.person));
+        lstContact.add(new Contact("FFF","654321",R.drawable.person2));
+
+
+    }
+}
---
+++ Week05/hw_week6_SDH/app/src/main/java/com/example/week6_sdh/MainActivity.java
@@ -0,0 +1,155 @@
+package com.example.week6_sdh;
+
+import android.os.Build;
+import android.os.Bundle;
+import android.support.annotation.RequiresApi;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.ListView;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+    TextView camera,chat,status,call;
+    ViewPager ViewPager;
+    PagerViewAdapter PagerViewAdapter;
+    ListView listView;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        camera = (TextView)findViewById(R.id.camera);
+        chat = (TextView)findViewById(R.id.chat);
+        status = (TextView)findViewById(R.id.status);
+        call = (TextView)findViewById(R.id.call);
+        ViewPager = (ViewPager)findViewById(R.id.fragment_container);
+        PagerViewAdapter = new PagerViewAdapter(getSupportFragmentManager());
+
+
+        ViewPager.setAdapter(PagerViewAdapter);
+
+
+        camera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ViewPager.setCurrentItem(0);
+            }
+        });
+
+        chat.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ViewPager.setCurrentItem(1);
+            }
+        });
+
+        status.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ViewPager.setCurrentItem(2);
+            }
+        });
+
+        call.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+                ViewPager.setCurrentItem(3);
+            }
+        });
+
+
+
+        ViewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int i, float v, int i1) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+
+                onChangeTab(position);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
+
+    }
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
+    private void onChangeTab(int position) {
+        if(position == 0)
+        {
+            camera.setTextSize(22);
+            camera.setTextColor(getColor(R.color.bright_color));
+
+            chat.setTextSize(20);
+            chat.setTextColor(getColor(R.color.Light_color));
+
+            status.setTextSize(20);
+            status.setTextColor(getColor(R.color.Light_color));
+
+
+            call.setTextSize(20);
+            call.setTextColor(getColor(R.color.Light_color));
+
+        }
+        if(position == 1)
+        {
+            camera.setTextSize(20);
+            camera.setTextColor(getColor(R.color.Light_color));
+
+            chat.setTextSize(22);
+            chat.setTextColor(getColor(R.color.bright_color));
+
+            status.setTextSize(20);
+            status.setTextColor(getColor(R.color.Light_color));
+
+
+            call.setTextSize(20);
+            call.setTextColor(getColor(R.color.Light_color));
+
+        }
+        if(position == 2)
+        {
+            camera.setTextSize(20);
+            camera.setTextColor(getColor(R.color.Light_color));
+
+            chat.setTextSize(20);
+            chat.setTextColor(getColor(R.color.Light_color));
+
+            status.setTextSize(22);
+            status.setTextColor(getColor(R.color.bright_color));
+
+
+            call.setTextSize(20);
+            call.setTextColor(getColor(R.color.Light_color));
+
+        }
+        if(position == 3)
+        {
+            camera.setTextSize(20);
+            camera.setTextColor(getColor(R.color.Light_color));
+
+            chat.setTextSize(20);
+            chat.setTextColor(getColor(R.color.Light_color));
+
+            status.setTextSize(20);
+            status.setTextColor(getColor(R.color.Light_color));
+
+
+            call.setTextSize(22);
+            call.setTextColor(getColor(R.color.bright_color));
+
+        }
+    }
+}
